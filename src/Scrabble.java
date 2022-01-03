05d159006369d99615742561121a324f5d10b8fd
Tim <72501085+TimAngevare@users.noreply.github.com>
2022-01-03 15:11:52 +0100

    Added tilebag.

--- src/Scrabble.java
+++ src/Scrabble.java
@@ -1,8 +1,56 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 public class Scrabble {
+    private HashMap tileBag;
     public static void main(String[] args) {
         System.out.println("Nothing happens but you did try playing!");
     }
 
+    public Scrabble(){
+        generateTiles();
+    }
+
+    public HashMap getTileBag(){
+        return tileBag;
+    }
+    public void deleteTile(Tile tile ){
+        // Wrm werkt dit niet?
+        Integer quanitity = this.tileBag.get(tile.getLetter());
+        tileBag.replace(tile.getLetter(), quanitity - 1);
+
+    }
+    private void generateTiles(){
+        this.tileBag = new HashMap<Character, Integer>();
+        tileBag.put('a', 9);
+        tileBag.put('b', 2);
+        tileBag.put('c', 2);
+        tileBag.put('d', 4);
+        tileBag.put('e', 12);
+        tileBag.put('f', 2);
+        tileBag.put('g', 2);
+        tileBag.put('h', 2);
+        tileBag.put('i', 8);
+        tileBag.put('j', 2);
+        tileBag.put('k', 2);
+        tileBag.put('l', 4);
+        tileBag.put('m', 2);
+        tileBag.put('n', 6);
+        tileBag.put('o', 8);
+        tileBag.put('p', 2);
+        tileBag.put('q', 1);
+        tileBag.put('r', 6);
+        tileBag.put('s', 4);
+        tileBag.put('t', 6);
+        tileBag.put('u', 4);
+        tileBag.put('v', 2);
+        tileBag.put('w', 2);
+        tileBag.put('x', 1);
+        tileBag.put('y', 2);
+        tileBag.put('z', 1);
+        tileBag.put(' ', 2);
+    }
     /**
      * Returns the correct letter value according to official scrabble rules
      * @param letter the letter you want to know the value of
@@ -32,6 +80,8 @@ public class Scrabble {
             case 'q':
             case 'z':
                 return 10;
+            case ' ':
+                return 0;
             default:
                 return 1;
         }
