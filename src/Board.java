c55259d75618cab7a9ec2b2f8ea562c3aa342f05
Jelle <jelle@famvandenbrink.net>
2022-01-03 11:42:01 +0100

    index vanaf 0, geen hardcoding

--- src/Board.java
+++ src/Board.java
@@ -1,35 +1,35 @@
-import java.sql.Array;
-
 public class Board {
-    private Square[][] board;
-    private final int length;
-    private final int mid;
+    private SquareType[][] board;
+    private static final int LENGTH = 15;
 
     public Board() {
-        fill();
-        this.length = 15;
-        this.mid = length / 2;
+        board = fill();
     }
 
-    private Square[][] fill(){
-        this.board = new Square[length][length];
-        for (int x = 1; x <= length; x++){
-            for (int y = 1; y <= length; y++){
-                if (x == mid && y == mid){
-                    board[x][y] = Square.START;
-                } else if ((x < 1 && x < 6 && y == x) || (x < 10 && x < 15 && x == y)) {
-                    board[x][y] = Square.DOUBLE_WORD;
-                } else if (((x == 1 || x == length) && (y == 1 || y == length || y == mid)) || (x == mid && (y == 1 || y == length))){
-                    board[x][y] = Square.TRIPLE_WORD;
-                } else if (((y == 2 || y == 10) && (x == 6 || x == 10)) || ((y == 6 || y == 10) && x % 4 == 2)){
-                    board[x][y] = Square.TRIPLE_LETTER;
-                } else if (((x == 1 || x == length) && (y == 4 || y == 12)) || ((x == 3 || x == 13) && (y == length - 1 || y == length + 1)) || ((x == mid - 4 || x == mid + 4) && (y == 1 || y == length || y == mid)) || ((x == mid - 1 || x == mid + 1) && (y == 3 || y == length - 3 || y == mid + 1 || y == mid - 1)) || (x == mid && (y == mid - (mid / 2) || y == mid + (mid / 2)))){
-                    board[x][y] = Square.DOUBLE_LETTER;
-                } else {
-                    board[x][y] = Square.BLANK;
-                }
+    private SquareType[][] fill(){
+        this.board = new SquareType[LENGTH][LENGTH];
+        for (int x = 0; x < LENGTH; x++){
+            for (int y = 0; y < LENGTH; y++){
+                board[x][y] = checkSquareType(x, y);
             }
         }
         return board;
+    }
+
+    public SquareType checkSquareType(int x, int y) {
+        int mid = Math.round(LENGTH / 2);
+        if (x == mid && y == mid){
+            return SquareType.START;
+        } else if (y == x && ((x > 0 && x < mid - 2) || (x > mid + 2 && x < LENGTH - 2))) {
+            return SquareType.DOUBLE_WORD;
+        } else if ((x == 0 || x == LENGTH - 1 || x == mid) && (y == 0 || y == LENGTH - 1 || y == mid)){
+            return SquareType.TRIPLE_WORD;
+        } else if ((x == mid + 2 || x == mid - 2) && (y == 1 || y == LENGTH - 2) || ((y == mid + 2 || y == mid - 2) && x % 4 == 1)) {
+            return SquareType.TRIPLE_LETTER;
+        } else if (((x == mid + 1 || x == mid - 1) && (y == mid - 1 || y == mid + 1)) || ((x == mid - 4 || y == mid - 4 || x == mid + 4 || y == mid + 4) && (y == 0 || x == 0)) || ((y == mid - 1 || y == mid + 1) && (x == 2 || x == LENGTH - 3)) || ((x == mid - 1 || x == mid + 1) && (y == 2 || y == LENGTH - 3)) || (y == mid && (x == 3 || x == LENGTH - 4))|| (x == mid && (y == 3 || y == LENGTH - 4)) ) {
+            return SquareType.DOUBLE_LETTER;
+        } else {
+            return SquareType.BLANK;
+        }
     }
 }
--- src/SquareType.java
+++ src/SquareType.java
@@ -1,3 +1,3 @@
-public enum Square {
+public enum SquareType {
     BLANK, DOUBLE_WORD, DOUBLE_LETTER, TRIPLE_WORD, TRIPLE_LETTER, START;
 }
